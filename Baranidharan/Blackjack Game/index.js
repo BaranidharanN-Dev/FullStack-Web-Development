e9a8e2fde865610e749a200eadf1f6be2520d573
BaranidharanN-Dev <143250325+BaranidharanN-Dev@users.noreply.github.com>
2026-09-03 13:39:31 +0530

    "Blackjack Js Project"

---
+++ Baranidharan/Blackjack Game/index.js
@@ -0,0 +1,28 @@
+let firstCard = 10
+let secondCard = 555
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = " "
+let output = document.getElementById("output")
+let messageEl = document.getElementById("message-el")
+// let sumEl = document.getElementById("sum-el")
+let sumEl = document.querySelector("#sum-el")
+
+// 2. Create a startGame() function. Move the conditional
+// below (line 11-20) inside the body of the function.
+
+function startGame() {
+
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+    message = "Do you want to draw a new card? 🙂"
+} else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳"
+    hasBlackJack = true
+} else {
+    message = "You're out of the game! 😭"
+    isAlive = false
+}
+messageEl.textContent = message
+}
