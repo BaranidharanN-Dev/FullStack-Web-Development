977e3463ecbcc334b0537b8011ccefeb1195f8de
BaranidharanN-Dev <143250325+BaranidharanN-Dev@users.noreply.github.com>
2026-09-09 10:37:15 +0530

    "update"

--- Scrim-s01uaapv2/index.js
+++ Scrim-s01uaapv2/index.js
@@ -1,42 +1,21 @@
-let myLeads = []
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-database.js";
+
+
+const firebaseConfig = { 
+    databaseURL: "https://leads-tracker-app-a4af0-default-rtdb.asia-southeast1.firebasedatabase.app/"
+ } 
+
+ 
+
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+const referenceInDB = ref(database, "leads")
+
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-const tabBtn = document.getElementById("tab-btn")
-
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
-}
-
-
-
-tabBtn.addEventListener("click", function(){
-    // Save the url instead of logging it out
-    // Source - https://stackoverflow.com/a/17826527
-// Posted by Jonathan Dumaine, modified by community. See post 'Timeline' for change history
-// Retrieved 2026-09-07, License - CC BY-SA 4.0
-
-//chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-  // since only one tab should be active and in the current window at once
-  // the return variable should only have one entry
-  
-  chrome.tabs.query({active:true, currentWindow: true }, function (tabs) { 
-    myLeads.push(tabs[0].url)
-   localStorage.setItem(myLeads, JSON.stringify(myLeads))
-   render(myLeads)
-    
-
-});
-  
-  
-
-
-   
-    
-})
 
 function render(leads) {
     let listItems = ""
@@ -53,14 +32,11 @@ function render(leads) {
 }
 
 deleteBtn.addEventListener("dblclick", function() {
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
+    
 })
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
+    push(referenceInDB,inputEl.value)
+    // Challenge: Import the 'push' function and modify the line above to push inputEl.value to the referenceInDB in the database
     inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    render(myLeads)
 })
